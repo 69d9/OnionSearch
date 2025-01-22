dbf8c478bb940e9ecb03497ff18b87fdb01d8a92
GhosT LulzSec <97871304+69d9@users.noreply.github.com>
2025-01-22 21:44:21 +0300

    Update Search.py

--- Search.py
+++ Search.py
@@ -5,6 +5,14 @@ import re
 from sys import exit
 from colorama import Fore, Style
 
+import signal
+
+def signal_handler(sig, frame):
+    print(f"\n{Fore.RED}Process interrupted by user. Exiting...{Style.RESET_ALL}")
+    exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 print(rf"""
 {Fore.YELLOW}
     Coded By GhosT LulzSec
@@ -28,40 +36,44 @@ hjw \  |         |  /
   o   o   o      o   o    o  
 """)
 
-search = input(f"{Fore.CYAN}Please enter a keyword to search for onion links: {Style.RESET_ALL}")
 try:
+    search = input(f"{Fore.CYAN}Please enter a keyword to search for onion links: {Style.RESET_ALL}")
     number = int(input(f"{Fore.CYAN}How many onion links would you like to retrieve? {Style.RESET_ALL}"))
-except Exception as err:
-    print(f"{Fore.RED}Error: {err}{Style.RESET_ALL}")
+
+    if " " in search:
+        search = search.replace(" ", "+")
+
+    url = f"https://ahmia.fi/search/?q={search}"
+
+    with open("user-agents.txt", "r+") as a:
+        users = random.choice(a.readlines()).strip()
+
+    header = {
+        "User-Agent": users
+    }
+
+    results_limt = number
+    result_collected = 0
+
+    webs = requests.get(url, headers=header).text
+    reg = r"\w+\.onion"
+    data = re.findall(reg, webs)
+    filename = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(5))
+    data = list(dict.fromkeys(data))
+
+    with open(f"{filename}.txt", "a+") as f:
+        for i in data:
+            if result_collected >= results_limt:
+                break
+            i = i + "\n"
+            f.write(i)
+            result_collected += 1
+
+    print(f"{Fore.BLUE}\nProcess completed successfully! All results have been saved in: {filename}.txt{Style.RESET_ALL}")
+
+except KeyboardInterrupt:
+    print(f"\n{Fore.RED}Process interrupted by user. Exiting...{Style.RESET_ALL}")
     exit(0)
-
-if " " in search:
-    search = search.replace(" ", "+")
-
-url = f"https://ahmia.fi/search/?q={search}"
-
-with open("user-agents.txt", "r+") as a:
-    users = random.choice(a.readlines()).strip()
-
-header = {
-    "User-Agent": users
-}
-
-results_limt = number
-result_collected = 0
-
-webs = requests.get(url, headers=header).text
-reg = r"\w+\.onion"
-data = re.findall(reg, webs)
-filename = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(5))
-data = list(dict.fromkeys(data))
-
-with open(f"{filename}.txt", "a+") as f:
-    for i in data:
-        if result_collected >= results_limt:
-            break
-        i = i + "\n"
-        f.write(i)
-        result_collected += 1
-
-print(f"{Fore.BLUE}\nProcess completed successfully! All results have been saved in: {filename}.txt{Style.RESET_ALL}")
+except Exception as e:
+    print(f"{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
+    exit(1)
